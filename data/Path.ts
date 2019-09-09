5d636fc65e16c908a2478f0ee1a99198573de15c
happy <happy>
2019-09-09 14:48:28 +0800

    路径修改

--- data/Path.ts
+++ data/Path.ts
@@ -3,10 +3,10 @@
 */
 module gamedating.data {
 	export class Path {
-		static ui_dating: string = "datingnqp_ui/";
-		static ui_dating_tongyong: string = "datingnqp_ui/tongyong/";
-		static atlas_dating_ui: string = "datingnqp_res/atlas/datingnqp_ui/";
-		static atlas_dating_ui_tongyong: string = "datingnq_res/atlas/datingnqp_ui/";
-		static sk_dating: string = 'datingnqp_ui/sk/';
+		static ui_dating: string = "dating_ui/";
+		static ui_dating_tongyong: string = "dating_ui/tongyong/";
+		static atlas_dating_ui: string = "dating_res/atlas/dating_ui/";
+		static atlas_dating_ui_tongyong: string = "dating_res/atlas/dating_ui/";
+		static sk_dating: string = 'dating_ui/sk/';
 	}
 }
