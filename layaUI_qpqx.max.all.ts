84727d71b8f97a99690257302d1745728103aa25
happy <happy>
2019-09-25 12:57:40 +0800

    UI导出

--- layaUI_qpqx.max.all.ts
+++ layaUI_qpqx.max.all.ts
@@ -1,13 +1,13 @@
 
 module ui.qpqx.dating {
     export class DengLuUI extends View {
-		public box_qita:Laya.Box;
 		public btn_wx:Laya.Button;
 		public btn_denglu:Laya.Button;
 		public btn_youke:Laya.Box;
 		public img_btn:Laya.Image;
+		public box_qita:Laya.Box;
 
-        public static  uiView:any ={"type":"View","props":{"y":0,"width":1280,"height":720},"child":[{"type":"Box","props":{"right":-1,"left":-1,"height":96,"bottom":60,"anchorY":0.5,"anchorX":0.5},"child":[{"type":"Box","props":{"width":204,"right":0,"height":108,"centerY":0},"child":[{"type":"Box","props":{"y":0,"x":0,"var":"box_qita"},"child":[{"type":"Image","props":{"skin":"dating_ui/denglurk/tu_dldi.png"}},{"type":"Image","props":{"y":6,"x":44,"skin":"dating_ui/denglurk/tu_dlfs.png"}}]},{"type":"Button","props":{"y":64,"x":140,"var":"btn_wx","stateNum":1,"skin":"dating_ui/denglurk/btn_weixin.png","anchorY":0.5,"anchorX":0.5}},{"type":"Button","props":{"y":64,"x":53,"width":63,"var":"btn_denglu","stateNum":1,"skin":"dating_ui/denglurk/btn_shouji.png","height":64,"anchorY":0.5,"anchorX":0.5}}]},{"type":"Box","props":{"y":-9,"width":329,"var":"btn_youke","height":120,"centerX":0},"child":[{"type":"Button","props":{"y":55,"x":162,"stateNum":1,"skin":"dating_ui/denglurk/btn_ksdl.png","anchorY":0.5,"anchorX":0.5}},{"type":"Image","props":{"x":1,"var":"img_btn","skin":"dating_ui/denglurk/tu_kskai.png"}}]}]}]};
+        public static  uiView:any ={"type":"View","props":{"y":0,"width":1280,"height":720},"child":[{"type":"Box","props":{"right":-1,"left":-1,"height":96,"bottom":60,"anchorY":0.5,"anchorX":0.5},"child":[{"type":"Button","props":{"y":48.5,"var":"btn_wx","stateNum":1,"skin":"dating_ui/denglurk/btn_weixin.png","centerX":450,"anchorY":0.5,"anchorX":0.5}},{"type":"Button","props":{"y":48.5,"var":"btn_denglu","stateNum":1,"skin":"dating_ui/denglurk/btn_shouji.png","centerX":280,"anchorY":0.5,"anchorX":0.5}},{"type":"Box","props":{"y":50,"x":642,"var":"btn_youke","centerX":0,"anchorY":0.5,"anchorX":0.5},"child":[{"type":"Button","props":{"width":284,"stateNum":1,"skin":"dating_ui/denglurk/btn_1canh.png","height":87,"centerY":0,"centerX":0},"child":[{"type":"Image","props":{"var":"img_btn","skin":"dating_ui/denglurk/tu_ksks.png","centerY":-8,"centerX":-1,"anchorY":0.5,"anchorX":0.5}}]}]},{"type":"Box","props":{"y":-12,"x":1138,"var":"box_qita"}}]}]};
         constructor(){ super()}
         createChildren():void {
         
