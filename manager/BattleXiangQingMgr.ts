283f978194d566fafc2329c20d351342db5855df
happy <happy>
2019-10-08 19:09:01 +0800

    牌局详情修改

--- manager/BattleXiangQingMgr.ts
+++ manager/BattleXiangQingMgr.ts
@@ -33,10 +33,12 @@ module gamedating.managers {
 			}
 		}
 
-		private _dataInfoList: { [key: string]: string } = {};
+		private _dataInfoList: { [key: string]: any } = {};
 		private _mapInfoList: { [key: string]: MapInfoT<MapInfoLogObject> } = {}
+		private _cardroomList: string[] = ["rniuniu", "rddz", "rpaodekuai", "rshisanshui"];
 		//战斗日志转成字符串
-		private GetBattleInfoToString(game_id: string, battle_log: string): string {
+		private GetBattleInfoToString(game_id: string, battle_log: string): any {
+			game_id = "rniuniu"
 			if (!this._mapInfoList[game_id]) {
 				let mapid = game_id.substr(0, 1).toUpperCase() + game_id.substr(1, game_id.length);
 				let comm = StringU.substitute("new game{0}.data.{1}MapInfo({2})", game_id, mapid, "this._game.sceneGame.sceneObjectMgr")
@@ -44,7 +46,11 @@ module gamedating.managers {
 			}
 
 			this._mapInfoList[game_id].battleInfoMgr.ResetData(battle_log);
-			return this._mapInfoList[game_id].getBattleInfoToString();
+			if (this._cardroomList.indexOf(game_id) != -1) {//房卡类型
+				return this._mapInfoList[game_id].getBattleInfoToObj();
+			} else {//非房卡类型
+				return this._mapInfoList[game_id].getBattleInfoToString();
+			}
 		}
 	}
 }
