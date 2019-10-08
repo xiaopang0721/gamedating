3b219afd7db6c693c2179364dd0616fe6db82db1
atubo <atubo@h.com>
2019-10-08 13:09:35 +0800

    1133 【个人中心】游戏设置里的音乐和音效还没有关到底 后面按钮就关闭了

--- page/GeRenXinXiPage.ts
+++ page/GeRenXinXiPage.ts
@@ -277,13 +277,13 @@ module gamedating.page {
 		private initVolume() {
 			this._viewUI.hslider0.min = 0;//设置 this.hslider0 最低位置值。
 			this._viewUI.hslider0.max = 1;//设置 this.hslider0 最高位置值。
-			this._viewUI.hslider0.tick = 0.1;//设置 this.hslider0 刻度值。
+			this._viewUI.hslider0.tick = 0.01;//设置 this.hslider0 刻度值。
 			this._viewUI.hslider0.changeHandler = new Handler(this, this.onChange0);//设置 this.hslider0 位置变化处理器。
 			this._viewUI.hslider0.value = Laya.SoundManager.soundVolume;//设置 this.hslider0 当前位置值。
 
 			this._viewUI.hslider1.min = 0;//设置 this.hslider0 最低位置值。
 			this._viewUI.hslider1.max = 1;//设置 this.hslider0 最高位置值。
-			this._viewUI.hslider1.tick = 0.1;//设置 this.hslider0 刻度值。
+			this._viewUI.hslider1.tick = 0.01;//设置 this.hslider0 刻度值。
 			this._viewUI.hslider1.changeHandler = new Handler(this, this.onChange1);//设置 this.hslider0 位置变化处理器。
 			this._viewUI.hslider1.value = Laya.SoundManager.musicVolume;//设置 this.hslider0 当前位置值。
 		}
@@ -297,6 +297,7 @@ module gamedating.page {
 			}
 			Laya.SoundManager.setSoundVolume(value);
 			localSetItem("soundVolume", value.toString());
+			logd('onChange0:', value);
 		}
 		private onChange1(value: number) {
 			value = parseFloat(value.toFixed(2));
@@ -307,6 +308,7 @@ module gamedating.page {
 			}
 			Laya.SoundManager.setMusicVolume(value);
 			localSetItem("musicVolume", value.toString());
+			logd('onChange1:', value);
 		}
 
 
