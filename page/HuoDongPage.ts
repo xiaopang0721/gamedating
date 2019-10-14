90f96504cfcdbbdb7a90a3d8316ad5df13af0133
xiaoer <xiaoer@example.com>
2019-10-14 16:23:27 +0800

    活动加载大图模式调整

--- page/HuoDongPage.ts
+++ page/HuoDongPage.ts
@@ -33,6 +33,8 @@ module gamedating.page {
 			this._viewUI.myhd0.vScrollBarSkin = "";
 			this._viewUI.myhd1.vScrollBarSkin = "";
 			this._viewUI.myhd2.vScrollBarSkin = "";
+			// this._viewUI.myhd2.itemRender = this.createChildren("dating.component.ChongZhi_IMGUI", ChongzhiImgRender);
+			// this._viewUI.myhd2.renderHandler = new Handler(this, this.renderImgHandler);
 			this._viewUI.txt.text = "";
 			this._viewUI.txt_myhd.text = "";
 
@@ -69,20 +71,29 @@ module gamedating.page {
 				this._viewUI.myhd0.visible = false;
 				this._viewUI.myhd1.visible = false;
 				this._viewUI.myhd2.visible = false;
-				if (selectedItem.img_url) {
+				if (selectedItem) {
+					let listData = selectedItem.img_list ? JSON.parse(selectedItem.img_list) : "";
 					if (selectedItem.pro_type == 1) {
 						//图文
 						this._viewUI.myhd1.visible = true;
-						this._viewUI.img_myhd.skin = selectedItem.img_url;
+						this._viewUI.img_myhd.skin = listData > 0 ? listData[0] : '';
 						this._viewUI.txt_myhd.text = selectedItem.content;
 						this._viewUI.txt_myhd.height = this._viewUI.txt_myhd.textField.textHeight;
 						this._viewUI.myhd1.height = selectedItem.topopup > 1 ? 425 : 510;
 					} else {
 						//纯大图
 						this._viewUI.myhd2.visible = true;
-						this._viewUI.img_myhd1.skin = selectedItem.img_url;
-						this._viewUI.img_myhd1.height = selectedItem.topopup > 1 ? 425 : 510;
 						this._viewUI.myhd2.height = selectedItem.topopup > 1 ? 425 : 510;
+						//排序数据
+						if (listData) {
+							listData.sort((a: any, b: any) => {
+								return Number(a.sort) - Number(b.sort);
+							});
+							this.clerarAll();
+							this._listData = listData;
+							//预加载
+							this.firstLoadUrl();
+						}
 					}
 				} else {
 					//纯文本
@@ -93,6 +104,76 @@ module gamedating.page {
 				}
 				this._viewUI.img_bg.visible = selectedItem.topopup > 1;
 				this._viewUI.btn_qiandao.visible = selectedItem.topopup > 1;
+			}
+		}
+
+		//资源预加载
+		private _listData;
+		private _refAssetArr: Array<RefAsset> = [];
+		private firstLoadUrl(): void {
+			let idx = 0;
+			for (let i = 0; i < this._listData.length; i++) {
+				let imgData = this._listData[i];
+				if (imgData) {
+					if (!imgData.path) return;
+					let refAsset = RefAsset.Get(imgData.path);
+					refAsset.retain();
+					this._refAssetArr.push(refAsset);
+					if (!refAsset.parseComplete) {
+						refAsset.once(LEvent.COMPLETE, this, () => {
+							//首次全部加载完成
+							idx++;
+							if (idx == this._listData.length) {
+								this.addImg();
+							}
+						});
+					}
+					else {
+						idx++;
+						if (idx == this._listData.length) {
+							this.addImg();
+						}
+					}
+				}
+			}
+		}
+		//添加图片
+		private _imgArr: Array<LImage> = [];
+		private addImg(): void {
+			if (this._viewUI) {
+				let y = 0;
+				for (let i = 0; i < this._listData.length; i++) {
+					let img = new LImage(this._listData[i].path);
+					this._imgArr.push(img);
+					img.width = 837;
+					img.pos(0, y)
+					this._viewUI.myhd2.addChild(img);
+					y += img.height;
+				}
+			}
+		}
+		private clerarAll(): void {
+			if (this._imgArr && this._imgArr.length > 0) {
+				//清除
+				for (let i = 0; i < this._imgArr.length; i++) {
+					let img: LImage = this._imgArr[i];
+					if (img) {
+						this._imgArr.splice(i, 1);
+						img.removeSelf();
+						img = null;
+						i--;
+					}
+				}
+			}
+			if (this._refAssetArr && this._refAssetArr.length > 0) {
+				for (let i = 0; i < this._refAssetArr.length; i++) {
+					let refAsset = this._refAssetArr[i];
+					if (refAsset) {
+						refAsset.offAll();
+						refAsset.release();
+						refAsset = null;
+					}
+				}
 			}
 		}
 
@@ -125,12 +206,12 @@ module gamedating.page {
 
 		public close(): void {
 			if (this._viewUI) {
+				this.clerarAll();
 				this._game.sceneGame.sceneObjectMgr.off(SceneObjectMgr.EVENT_PLAYER_INFO_UPDATE, this, this.getData);
 			}
 			super.close();
 		}
 	}
-
 
 	class TabItemRender extends ui.nqp.dating.component.TabItemRender1UI {
 		private _game: Game;
