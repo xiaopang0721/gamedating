0d3ed1bdd48d46a71646e92e42de5c46e16ab9ee
xiaoer <xiaoer@example.com>
2019-10-16 21:59:21 +0800

    提交公告挪动到活动那边

--- page/MessagePage.ts
+++ page/MessagePage.ts
@@ -23,23 +23,13 @@ module gamedating.page {
 			this._viewUI = this.createView("dating.XinXiUI");
 			this.addChild(this._viewUI);
 
-			this._viewUI.list_gonggao.vScrollBarSkin = "";
-			this._viewUI.list_gonggao.scrollBar.autoHide = false;
-			this._viewUI.list_gonggao.scrollBar.elasticDistance = 100;
-			this._viewUI.list_gonggao.itemRender = this.createChildren("dating.XinXi_lb2UI", GongGaoItemRender);
-			this._viewUI.list_gonggao.renderHandler = new Handler(this, this.renderHandler);
-			this._viewUI.list_gonggao.dataSource = [];
-
 			this._viewUI.list_mail.vScrollBarSkin = "";
 			this._viewUI.list_mail.scrollBar.autoHide = false;
 			this._viewUI.list_mail.scrollBar.elasticDistance = 100;
 			this._viewUI.list_mail.itemRender = this.createChildren("dating.component.XinXi_lbUI", MailItemRender);
 			this._viewUI.list_mail.renderHandler = new Handler(this, this.renderHandler);
 			this._viewUI.list_mail.dataSource = [];
-
-			this._viewUI.tab.selectHandler = new Handler(this, this.tabSelectHandler);
-			this._viewUI.tab.selectedIndex = 0;
-			this._game.sceneGame.sceneObjectMgr.on(SceneObjectMgr.EVENT_OPRATE_SUCESS, this, this.onSucessHandler);
+			
 		}
 
 		// 页面打开时执行函数
@@ -47,40 +37,12 @@ module gamedating.page {
 			super.onOpen();
 			this._game.datingGame.mailMgr.on(MailMgr.EVENT_CHANGE, this, this.onUpdateMail);
 			this.onUpdateMail();
-			this._game.network.call_get_bulletin_list();
-		}
-
-		private onSucessHandler(data: any) {
-			if (data.code == Web_operation_fields.CLIENT_IRCODE_GETBULLETINLIST) {
-				if (data && data.success == 0) {//公告
-					let gonggao = data.msg && data.msg.list && data.msg.list.length > 0 ? data.msg.list : [];
-					for (let key in gonggao) {
-						this._activeList.push(gonggao[key]);
-					}
-					if (this._activeList) {
-						this._viewUI.list_gonggao.selectedIndex = -1
-						this._viewUI.list_gonggao.dataSource = this._activeList;
-						if (this._activeList && this._activeList.length > 0) {
-							this._viewUI.list_gonggao.selectedIndex = 0;
-						}
-						this._viewUI.list_gonggao.visible = this._activeList && this._activeList.length > 0;
-						this._viewUI.txt_no_gonggao.visible = !this._viewUI.list_gonggao.visible;
-						this._game.network.call_read_bulletin();
-					}
-				}
-			}
 		}
 
 		private renderHandler(cell: any, index: number) {
 			if (cell) {
 				cell.setData(this._game, cell.dataSource);
 			}
-		}
-
-		private _activeList: any[] = []
-		private tabSelectHandler(index: number) {
-			this._viewUI.box_gonggao.visible = index + 1 == MessagePage.TYPE_GONGGAO;
-			this._viewUI.box_mail.visible = index + 1 == MessagePage.TYPE_MAIL;
 		}
 
 		private onUpdateMail() {
@@ -94,33 +56,9 @@ module gamedating.page {
 
 		public close(): void {
 			if (this._viewUI) {
-				this._game.sceneGame.sceneObjectMgr.off(SceneObjectMgr.EVENT_OPRATE_SUCESS, this, this.onSucessHandler);
 				this._game.datingGame.mailMgr.off(MailMgr.EVENT_CHANGE, this, this.onUpdateMail);
 			}
 			super.close();
-		}
-	}
-
-	class GongGaoItemRender extends ui.nqp.dating.XinXi_lb2UI {
-		private _game: Game;
-		private _data: MailData;
-		setData(game: Game, data: any) {
-			this._game = game;
-			this._data = data;
-			this.txt_title.text = data.title;
-			this.txt_name.text = data.create_user;
-			TextFieldU.setHtmlText(this.txt_content, data.context || "点击查看详情");
-			this.txt_time.text = Sync.getTimeStr(data.create_time * 1000);
-			this.on(LEvent.CLICK, this, this.onMouseHandle);
-		}
-
-		private onMouseHandle() {
-			if (!this._data.isread) {
-				this._game.datingGame.mailMgr.readMail(this._data.mail_orderid);
-			}
-			this._game.uiRoot.general.open(DatingPageDef.PAGE_NEW_GONGGAO, (page: GongGaoOpenPage) => {
-				page.setData(this._data);
-			})
 		}
 	}
 
